97102580936fef9dff43a7a3ed07bad91ee24be5
Pedro Oliveira <pedro120403@gmail.com>
2024-05-13 11:42:33 -0300

    Conceito inicial da nav bar
    - Aplicação de classes do Bootstrap porque eu estou usando um modelo de navbar do próprio Bootstrap

--- src/components/navbar.vue
+++ src/components/navbar.vue
@@ -1,29 +1,42 @@
 <template>
-    <nav class="navbar navbar-expand-lg bg-body-tertiary bg-dark navbar-dark">
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
         <div class="container-fluid">
-            <a class="navbar-brand" href="#">Navbar</a>
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-            </button>
-            <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
-            <div class="navbar-nav">
-                <a class="nav-link active" aria-current="page" href="#">Home</a>
-                <a class="nav-link" href="#">Features</a>
-                <a class="nav-link" href="#">Pricing</a>
-                <a class="nav-link disabled" aria-disabled="true">Disabled</a>
-            </div>
-            </div>
+            <a class="navbar-brand" href="#">LOGO.PNG</a>
+            <ul class="navbar-nav position-absolute top-50 start-50 translate-middle">
+                <li v-for="page in pages" class="nav-item mx-4">
+                    <a class="nav-link" aria-current="page" :href="page.link.url">{{ page.link.name }}</a>
+                </li>
+            </ul>
         </div>
     </nav>
 </template>
 
 <script>
     export default{
+        props:['links'],
         data(){
             return{
-
+                pages: [
+                    {
+                        link: {name: 'Home', url: 'index.html'},
+                    },
+                    {
+                        link: {name: 'Paineis', url: 'panels.html'},
+                    },
+                    {
+                        link: {name: 'Projetos', url: 'projects.html'},
+                    },
+                    {
+                        link: {name: 'Serviços', url: 'services.html'},
+                    },
+                    {
+                        link: {name: 'Sobre nós', url: 'about.html'},
+                    },
+                    {
+                        link: {name: 'Contato', url: 'contact.html'},
+                    },
+                ]
             }
         }
     }
-
 </script>
