0197999a3b3a31f907fb6792cd4e6da49e605cfd
Pedro Oliveira <pedro120403@gmail.com>
2024-05-13 14:49:20 -0300

    Criação do carrossel e link com o App.vue
    - Criação de uma estrutura genérica e padrão para edição futura

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,16 @@
 <template>
     <navbar></navbar>
+    <photo-carousel></photo-carousel>
 </template>
 
 <script>
     import Navbar from './components/navbar.vue'
+    import PhotoCarousel from './components/photo-carousel.vue'
 
     export default{
         components: {
             Navbar,
+            PhotoCarousel
         },
         data(){
             return{
