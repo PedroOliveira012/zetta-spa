1c064e56297d2c2f5eaa74e54afdba9a435f2a7d
Pedro Oliveira <pedro120403@gmail.com>
2024-05-14 14:06:41 -0300

    Criação do carrossel de avaliações
    - Criação da estrutura para edição futura

--- src/App.vue
+++ src/App.vue
@@ -2,18 +2,21 @@
     <navbar></navbar>
     <photo-carousel></photo-carousel>
     <products-sections></products-sections>
+    <rating-carousel></rating-carousel>
 </template>
 
 <script>
     import Navbar from './components/navbar.vue'
     import PhotoCarousel from './components/photo-carousel.vue';
     import productsSections from './components/products-sections.vue';
+    import ratingCarousel from './components/rating-carousel.vue'
 
     export default{
         components: {
             Navbar,
             PhotoCarousel,
-            productsSections
+            productsSections,
+            ratingCarousel
         },
         data(){
             return{
