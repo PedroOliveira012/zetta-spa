32c55710275a0856858a5b708b450d2d442bbe32
Pedro Oliveira <pedro120403@gmail.com>
2024-05-14 14:15:33 -0300

    Remoção dos arquivos
    - Não necessário o uso desse componente

--- src/App.vue
+++ src/App.vue
@@ -2,21 +2,18 @@
     <navbar></navbar>
     <photo-carousel></photo-carousel>
     <products-sections></products-sections>
-    <rating-carousel></rating-carousel>
 </template>
 
 <script>
     import Navbar from './components/navbar.vue'
     import PhotoCarousel from './components/photo-carousel.vue';
     import productsSections from './components/products-sections.vue';
-    import ratingCarousel from './components/rating-carousel.vue'
 
     export default{
         components: {
             Navbar,
             PhotoCarousel,
             productsSections,
-            ratingCarousel
         },
         data(){
             return{
