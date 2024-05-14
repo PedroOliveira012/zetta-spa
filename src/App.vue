2ca08b1f786ee449e570aad6b58b409859374bf1
Pedro Oliveira <pedro120403@gmail.com>
2024-05-14 10:54:12 -0300

    Criação das sections
    - Criação da estrutura para edição futura

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,19 @@
 <template>
     <navbar></navbar>
     <photo-carousel></photo-carousel>
+    <products-sections></products-sections>
 </template>
 
 <script>
     import Navbar from './components/navbar.vue'
-import PhotoCarousel from './components/photo-carousel.vue';
-    import photoCarousel from './components/photo-carousel.vue';
+    import PhotoCarousel from './components/photo-carousel.vue';
+    import productsSections from './components/products-sections.vue';
 
     export default{
         components: {
             Navbar,
-            PhotoCarousel
+            PhotoCarousel,
+            productsSections
         },
         data(){
             return{
---
+++ src/components/products-sections.vue
@@ -0,0 +1,41 @@
+<template>
+    <div>
+        <section class="grid gap-4 grid-cols-2 grid-rows-1 content-center">
+            <div>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus eu iaculis velit, in malesuada odio. Morbi nec ligula ut lectus convallis efficitur. Nulla hendrerit mattis arcu, ac molestie enim tempor sed. Integer dui lectus, molestie eu varius vel, dignissim at odio. Quisque tristique dolor laoreet, semper tellus non, hendrerit metus.</p>
+                <button class="btn btn-primary">teste cta</button>
+            </div>
+            <div>
+                <p>imagem</p>
+            </div>
+        </section>
+        <section class="grid gap-4 grid-cols-2 grid-rows-1 content-center">
+            <div class="">
+                <p>imagem</p>
+            </div>
+            <div>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus eu iaculis velit, in malesuada odio. Morbi nec ligula ut lectus convallis efficitur. Nulla hendrerit mattis arcu, ac molestie enim tempor sed. Integer dui lectus, molestie eu varius vel, dignissim at odio. Quisque tristique dolor laoreet, semper tellus non, hendrerit metus.</p>
+                <button class="btn btn-primary">teste cta</button>
+            </div>
+        </section>
+        <section class="grid gap-4 grid-cols-2 grid-rows-1 content-center">
+            <div>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus eu iaculis velit, in malesuada odio. Morbi nec ligula ut lectus convallis efficitur. Nulla hendrerit mattis arcu, ac molestie enim tempor sed. Integer dui lectus, molestie eu varius vel, dignissim at odio. Quisque tristique dolor laoreet, semper tellus non, hendrerit metus.</p>
+                <button class="btn btn-primary">teste cta</button>
+            </div>
+            <div>
+                <p>imagem</p>
+            </div>
+        </section>
+    </div>
+</template>
+
+<script>
+    export default{
+        data(){
+            return{
+
+            }
+        }
+    }
+</script>
