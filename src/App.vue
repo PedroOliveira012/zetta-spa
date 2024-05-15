b74a1fc29974298445b276321d7b4dde4ed6e3c2
Pedro Oliveira <pedro120403@gmail.com>
2024-05-15 08:26:16 -0300

    Criação da section dos parceiros
    - - Criação da estrutura para edição futura

--- src/App.vue
+++ src/App.vue
@@ -2,18 +2,21 @@
     <navbar></navbar>
     <photo-carousel></photo-carousel>
     <products-sections></products-sections>
+    <partner-section></partner-section>
 </template>
 
 <script>
     import Navbar from './components/navbar.vue'
     import PhotoCarousel from './components/photo-carousel.vue';
     import productsSections from './components/products-sections.vue';
+    import partnerSection from './components/partner-section.vue'
 
     export default{
         components: {
             Navbar,
             PhotoCarousel,
             productsSections,
+            partnerSection,
         },
         data(){
             return{
