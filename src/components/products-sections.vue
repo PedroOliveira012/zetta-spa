b5f34016711f353c78376d4f7915289ab83b09c2
Pedro Oliveira <pedro120403@gmail.com>
2024-05-16 16:32:13 -0300

    Estilização de componentes
    - Estilização das partes do componente products-section

--- src/components/products-sections.vue
+++ src/components/products-sections.vue
@@ -1,29 +1,39 @@
 <template>
-    <div>
-        <section class="grid gap-4 grid-cols-2 grid-rows-1 content-center">
-            <div>
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus eu iaculis velit, in malesuada odio. Morbi nec ligula ut lectus convallis efficitur. Nulla hendrerit mattis arcu, ac molestie enim tempor sed. Integer dui lectus, molestie eu varius vel, dignissim at odio. Quisque tristique dolor laoreet, semper tellus non, hendrerit metus.</p>
-                <button class="btn btn-primary">teste cta</button>
+    <div class="grid gap-4 grid-cols-1 grid-rows-3 place-content-evenly mt-24 mb-24">
+        <section class="grid gap-4 grid-cols-2 grid-rows-1 content-center m-auto pl-36 pr-36 pb-32 h-12/12">
+            <div class="w-12/12  relative">
+                <h3 class="text-6xl mb-6 pt-12">
+                    Lorem ipsum dolor sit amet,  CIMA 
+                </h3>
+                <p class="text-2xl pb-12">consectetur adipiscing elit. Vivamus eu iaculis velit, in malesuada odio. Morbi nec ligula ut lectus convallis efficitur. Nulla hendrerit mattis arcu, ac molestie enim tempor sed. Integer dui lectus, molestie eu varius vel, dignissim at odio. Quisque tristique dolor laoreet, semper tellus non, hendrerit metus.</p>
+                <button class="btn absolute bottom-0 right-0 mr-8 mb-8 bg-[#00aac0]">teste cta</button>
             </div>
-            <div>
+            <div class="border-2 rounded-r-full">
                 <p>imagem</p>
             </div>
         </section>
-        <section class="grid gap-4 grid-cols-2 grid-rows-1 content-center">
-            <div class="">
+        <section class="grid gap-4 grid-cols-2 grid-rows-1 content-center m-auto pl-36 pr-36 pb-24">
+            <div class="border-2 rounded-l-full">
                 <p>imagem</p>
             </div>
-            <div>
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus eu iaculis velit, in malesuada odio. Morbi nec ligula ut lectus convallis efficitur. Nulla hendrerit mattis arcu, ac molestie enim tempor sed. Integer dui lectus, molestie eu varius vel, dignissim at odio. Quisque tristique dolor laoreet, semper tellus non, hendrerit metus.</p>
-                <button class="btn btn-primary">teste cta</button>
+            <div class="w-12/12 relative">
+                <h3 class="text-6xl mb-6 pt-12">
+                    Lorem ipsum dolor sit amet, MEIO 
+                </h3>
+                <p class="text-2xl pb-12">consectetur adipiscing elit. Vivamus eu iaculis velit, in malesuada odio. Morbi nec ligula ut lectus convallis efficitur. Nulla hendrerit mattis arcu, ac molestie enim tempor sed. Integer dui lectus, molestie eu varius vel, dignissim at odio. Quisque tristique dolor laoreet, semper tellus non, hendrerit metus.</p>
+                <button class="btn absolute bottom-0 right-0 mr-8 mb-8 bg-[#00aac0]">teste cta</button>
             </div>
+            
         </section>
-        <section class="grid gap-4 grid-cols-2 grid-rows-1 content-center">
-            <div>
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus eu iaculis velit, in malesuada odio. Morbi nec ligula ut lectus convallis efficitur. Nulla hendrerit mattis arcu, ac molestie enim tempor sed. Integer dui lectus, molestie eu varius vel, dignissim at odio. Quisque tristique dolor laoreet, semper tellus non, hendrerit metus.</p>
-                <button class="btn btn-primary">teste cta</button>
+        <section class="grid gap-4 grid-cols-2 grid-rows-1 content-center m-auto pl-36 pr-36 pb-24">
+            <div class="w-12/12 relative">
+                <h3 class="text-6xl mb-6 pt-12">
+                    Lorem ipsum dolor sit amet, BAIXO 
+                </h3>
+                <p class="text-2xl pb-12">consectetur adipiscing elit. Vivamus eu iaculis velit, in malesuada odio. Morbi nec ligula ut lectus convallis efficitur. Nulla hendrerit mattis arcu, ac molestie enim tempor sed. Integer dui lectus, molestie eu varius vel, dignissim at odio. Quisque tristique dolor laoreet, semper tellus non, hendrerit metus.</p>
+                <button class="btn absolute bottom-0 right-0 mr-8 mb-8 bg-[#00aac0]">teste cta</button>
             </div>
-            <div>
+            <div class="border-2 rounded-r-full">
                 <p>imagem</p>
             </div>
         </section>
