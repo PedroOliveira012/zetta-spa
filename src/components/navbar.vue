f0fc67ccab29c236a654d1295bf3cad96315c35a
Pedro Oliveira <pedro120403@gmail.com>
2024-05-16 08:14:23 -0300

    Criação dos estilos da navbar

--- src/components/navbar.vue
+++ src/components/navbar.vue
@@ -1,9 +1,13 @@
 <template>
-    <nav class="navbar navbar-expand-lg">
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
         <div class="container-fluid">
-            <a class="navbar-brand" href="#">LOGO.PNG</a>
+            <a class="navbar-brand" href="#">
+                <div class="w-64">
+                    <img src="../assets/logo zetta.png">
+                </div>
+            </a>
             <ul class="navbar-nav position-absolute top-50 start-50 translate-middle">
-                <li v-for="page in pages" class="nav-item mx-4">
+                <li v-for="page in pages" class="nav-item mx-4 border-b-2 border-[#212529] hover:text-white hover:border-b-2 hover:border-[#00aac0] transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 duration-300">
                     <a class="nav-link" aria-current="page" :href="page.link.url">{{ page.link.name }}</a>
                 </li>
             </ul>
