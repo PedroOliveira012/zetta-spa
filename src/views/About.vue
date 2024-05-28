88bb8a4b409ddbf2c6f1b68278b07b1f1a04df8d
Pedro Oliveira <pedro120403@gmail.com>
2024-05-28 11:25:59 -0300

    Componente do about
    - Criação do componente da pagina de about

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,6 +1,11 @@
 <template>
-    <h1>About</h1>
+    <about />
 </template>
 <script>
-
+    import About from '@/components/about/about.vue'
+    export default{
+        components:{
+            About
+        }
+    }
 </script>
