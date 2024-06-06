5bd7ef84e70e81e303a1f9bdf772befbaf7ab637
Pedro Oliveira <pedro120403@gmail.com>
2024-06-06 10:46:41 -0300

    Formatação dos botões
    - Alteração do estilo dos botões

--- src/components/home/products-sections.vue
+++ src/components/home/products-sections.vue
@@ -7,19 +7,28 @@
                 </h3>
                 <p class="text-2xl pb-12">Lorem ipsum dolor sit amet consectetur adipiscing elit. Vivamus eu iaculis velit, in malesuada odio. Morbi nec ligula ut lectus convallis efficitur. Nulla hendrerit mattis arcu, ac molestie enim tempor sed. Integer dui lectus, molestie eu varius vel, dignissim at odio. Ut iaculis vestibulum nisl placerat gravida. Donec eget tempor metus, sit amet aliquet tellus.</p>
                 <router-link to="/panels">
-                    <button class="absolute bottom-0 right-0 inline-flex items-center justify-center p-0.5 mb-2 me-2 overflow-hidden text-sm font-medium rounded-lg group bg-gradient-to-br from-[#00daf3] to-[#0095a5] hover:shadow-2xl hover:-translate-y-1 hover:scale-110 duration-300">
+                    <!-- <button class="absolute bottom-0 right-0 inline-flex items-center justify-center p-0.5 mb-2 me-2 overflow-hidden text-sm font-medium rounded-lg group bg-gradient-to-br from-[#00daf3] to-[#0095a5] hover:shadow-2xl hover:-translate-y-1 hover:scale-110 duration-300">
                         <span class="relative px-5 py-2.5 transition-all ease-in duration-180 dark:bg-white rounded-md group-hover:bg-opacity-0">
+                            Saiba mais!
+                        </span>
+                    </button> -->
+                    <button class="absolute bottom-0 right-0 overflow-hidden w-32 p-2 h-12 bg-black text-white border-none rounded-md text-xl font-bold cursor-pointer z-10 group">
+                        Saiba mais!
+                        <span class="absolute w-36 h-32 -top-8 -left-2 bg-white rotate-12 transform scale-x-0 group-hover:scale-x-100 transition-transform group-hover:duration-500 duration-1000 origin-left"></span>
+                        <span class="absolute w-36 h-32 -top-8 -left-2 bg-cyan-400 rotate-12 transform scale-x-0 group-hover:scale-x-100 transition-transform group-hover:duration-700 duration-700 origin-left"></span>
+                        <span class="absolute w-36 h-32 -top-8 -left-2 bg-cyan-600 rotate-12 transform scale-x-0 group-hover:scale-x-100 transition-transform group-hover:duration-1000 duration-500 origin-left"></span>
+                        <span class="group-hover:opacity-100 group-hover:duration-1000 duration-100 opacity-5 absolute w-36 h-32 top-2.5 -left-2 z-10">
                             Saiba mais!
                         </span>
                     </button>
                 </router-link>
             </div>
-            <div class="border-2 rounded-r-full">
+            <div class="border-2 border-cyan-500 rounded-r-full">
                 <p>imagem</p>
             </div>
         </section>
         <section class="grid gap-4 grid-cols-2 grid-rows-1 content-center m-auto pl-36 pr-36 pb-24">
-            <div class="border-2 rounded-l-full">
+            <div class="border-2 border-cyan-500 rounded-l-full">
                 <p>imagem</p>
             </div>
             <div class="w-12/12 relative">
@@ -27,14 +36,21 @@
                     Projetos elétricos
                 </h3>
                 <p class="text-2xl pb-12">Lorem ipsum dolor sit amet consectetur adipiscing elit. Vivamus eu iaculis velit, in malesuada odio. Morbi nec ligula ut lectus convallis efficitur. Nulla hendrerit mattis arcu, ac molestie enim tempor sed. Integer dui lectus, molestie eu varius vel, dignissim at odio. Ut iaculis vestibulum nisl placerat gravida. Donec eget tempor metus, sit amet aliquet tellus.</p>
-                
-                    <button class="absolute bottom-0 right-0 inline-flex items-center justify-center p-0.5 mb-2 me-2 overflow-hidden text-sm font-medium rounded-lg group bg-gradient-to-br from-[#00daf3] to-[#0095a5] hover:shadow-2xl hover:-translate-y-1 hover:scale-110 duration-300">
-                        <span class="relative px-5 py-2.5 transition-all ease-in duration-180 dark:bg-white rounded-md group-hover:bg-opacity-0">
-                                <router-link to="/projects">
-                                    Saiba mais!
-                                </router-link>
-                            </span>
+                <router-link to="/projects">
+                    <button class="absolute bottom-0 right-0 overflow-hidden w-32 p-2 h-12 bg-black text-white border-none rounded-md text-xl font-bold cursor-pointer z-10 group">
+                        Saiba mais!
+                        <span class="absolute w-36 h-32 -top-8 -left-2 bg-white rotate-12 transform scale-x-0 group-hover:scale-x-100 transition-transform group-hover:duration-500 duration-1000 origin-left"></span>
+                        <span class="absolute w-36 h-32 -top-8 -left-2 bg-cyan-400 rotate-12 transform scale-x-0 group-hover:scale-x-100 transition-transform group-hover:duration-700 duration-700 origin-left"></span>
+                        <span class="absolute w-36 h-32 -top-8 -left-2 bg-cyan-600 rotate-12 transform scale-x-0 group-hover:scale-x-100 transition-transform group-hover:duration-1000 duration-500 origin-left"></span>
+                        <span class="group-hover:opacity-100 group-hover:duration-1000 duration-100 opacity-5 absolute w-36 h-32 top-2.5 -left-2 z-10">
+                            Saiba mais!
+                        </span>
                     </button>
+                </router-link>
+                <!-- <button class="absolute bottom-0 right-0 inline-flex items-center justify-center p-0.5 mb-2 me-2 overflow-hidden text-sm font-medium rounded-lg group bg-gradient-to-br from-[#00daf3] to-[#0095a5] hover:shadow-2xl hover:-translate-y-1 hover:scale-110 duration-300">
+                    <span class="relative px-5 py-2.5 transition-all ease-in duration-180 dark:bg-white rounded-md group-hover:bg-opacity-0">
+                    </span>
+                </button> -->
             </div>
             
         </section>
@@ -45,14 +61,23 @@
                 </h3>
                 <p class="text-2xl pb-12">Lorem ipsum dolor sit amet consectetur adipiscing elit. Vivamus eu iaculis velit, in malesuada odio. Morbi nec ligula ut lectus convallis efficitur. Nulla hendrerit mattis arcu, ac molestie enim tempor sed. Integer dui lectus, molestie eu varius vel, dignissim at odio. Ut iaculis vestibulum nisl placerat gravida. Donec eget tempor metus, sit amet aliquet tellus.</p>
                 <router-link to="/robots">
-                    <button class="absolute bottom-0 right-0 inline-flex items-center justify-center p-0.5 mb-2 me-2 overflow-hidden text-sm font-medium rounded-lg group bg-gradient-to-br from-[#00daf3] to-[#0095a5] hover:shadow-2xl hover:-translate-y-1 hover:scale-110 duration-300">
+                    <!-- <button class="absolute bottom-0 right-0 inline-flex items-center justify-center p-0.5 mb-2 me-2 overflow-hidden text-sm font-medium rounded-lg group bg-gradient-to-br from-[#00daf3] to-[#0095a5] hover:shadow-2xl hover:-translate-y-1 hover:scale-110 duration-300">
                         <span class="relative px-5 py-2.5 transition-all ease-in duration-180 dark:bg-white rounded-md group-hover:bg-opacity-0">
+                            Saiba mais!
+                        </span>
+                    </button> -->
+                    <button class="absolute bottom-0 right-0 overflow-hidden w-32 p-2 h-12 bg-black text-white border-none rounded-md text-xl font-bold cursor-pointer z-10 group">
+                        Saiba mais!
+                        <span class="absolute w-36 h-32 -top-8 -left-2 bg-white rotate-12 transform scale-x-0 group-hover:scale-x-100 transition-transform group-hover:duration-500 duration-1000 origin-left"></span>
+                        <span class="absolute w-36 h-32 -top-8 -left-2 bg-cyan-400 rotate-12 transform scale-x-0 group-hover:scale-x-100 transition-transform group-hover:duration-700 duration-700 origin-left"></span>
+                        <span class="absolute w-36 h-32 -top-8 -left-2 bg-cyan-600 rotate-12 transform scale-x-0 group-hover:scale-x-100 transition-transform group-hover:duration-1000 duration-500 origin-left"></span>
+                        <span class="group-hover:opacity-100 group-hover:duration-1000 duration-100 opacity-5 absolute w-36 h-32 top-2.5 -left-2 z-10">
                             Saiba mais!
                         </span>
                     </button>
                 </router-link>
             </div>
-            <div class="border-2 rounded-r-full">
+            <div class="border-2 border-cyan-500 rounded-r-full">
                 <p>imagem</p>
             </div>
         </section>
