ca94501387d5d52df9536046893d8d2595643508
Pedro Oliveira <pedro120403@gmail.com>
2024-06-10 15:29:56 -0300

    Inclusão do Vue Motion ao projeto

--- src/main.js
+++ src/main.js
@@ -1,5 +1,9 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from '../src/routes'
+import { MotionPlugin } from '@vueuse/motion'
 
-createApp(App).use(router).mount('#app')
+const app = createApp(App)
+app.use(router)
+app.use(MotionPlugin)
+app.mount('#app')
