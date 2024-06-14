bc260a6aa465cb3078a8d03e2438bfb785737152
Pedro Oliveira <pedro120403@gmail.com>
2024-06-14 09:56:27 -0300

    Estilização de componente
    - Centralização dos textos na página Sobre

--- src/components/about/about.vue
+++ src/components/about/about.vue
@@ -1,9 +1,9 @@
 <template>
     <section class="mb-16">
-        <div class="bg-[#777777] h-80 mb-16">banner</div>
+        <div class="bg-[#777777] h-96 mb-16">banner</div>
         <div class="flex flex-col justify-center items-center mb-12">
             <h3 class="text-4xl mb-6">Quem somos</h3>
-            <p class="text-xl w-6/12">
+            <p class="text-xl text-center w-6/12">
                 Fundada em 13 de setembro de 2006, a ZETTATECCK PROJETOS INDUSTRIAIS E AUTOMAÇÃO LTDA é uma empresa especializada na fabricação de painéis elétricos de baixa tensão, controle e instrumentação, robôs sinalizadores, e na elaboração de projetos elétricos industriais e automação predial.
                 Nossa missão é inovar constantemente e desenvolver soluções que não apenas reduzam custos, mas também aumentem a produtividade e a qualidade. Comprometemo-nos a mitigar riscos relacionados à segurança do trabalho, garantindo um ambiente mais seguro para todos.
                 Na ZETTATECCK, valorizamos a criatividade e a tecnologia em cada projeto, buscando sempre a satisfação permanente de nossos clientes. Acreditamos no crescimento contínuo e na prosperidade, tanto da empresa quanto da comunidade.
@@ -11,7 +11,7 @@
         </div>
         <div class="flex flex-col justify-center items-center mb-12">
             <h3 class="text-4xl mb-6">Nossa história</h3>
-            <p class="text-xl w-6/12">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut vel lorem lacus. Sed maximus massa ac euismod egestas. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Quisque ac dignissim nunc. Phasellus tincidunt tincidunt arcu, eu dignissim diam mattis in. Nulla eleifend turpis ut porta interdum. Nam non eros interdum, vehicula mi vel, tempus arcu. Donec a ipsum quis purus ullamcorper mattis non eu lacus. Duis ex velit, tristique et pretium eget, bibendum nec mauris. Donec dignissim rhoncus elit eget rhoncus. Donec facilisis libero sit amet justo blandit blandit. Vivamus eleifend sagittis lacus ut varius. Nulla arcu arcu, cursus et ornare et, euismod ac justo. Donec convallis rutrum tellus, in fringilla turpis mattis et.</p>
+            <p class="text-xl text-center w-6/12">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut vel lorem lacus. Sed maximus massa ac euismod egestas. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Quisque ac dignissim nunc. Phasellus tincidunt tincidunt arcu, eu dignissim diam mattis in. Nulla eleifend turpis ut porta interdum. Nam non eros interdum, vehicula mi vel, tempus arcu. Donec a ipsum quis purus ullamcorper mattis non eu lacus. Duis ex velit, tristique et pretium eget, bibendum nec mauris. Donec dignissim rhoncus elit eget rhoncus. Donec facilisis libero sit amet justo blandit blandit. Vivamus eleifend sagittis lacus ut varius. Nulla arcu arcu, cursus et ornare et, euismod ac justo. Donec convallis rutrum tellus, in fringilla turpis mattis et.</p>
         </div>
     </section>
     <section class="my-16">
@@ -19,7 +19,7 @@
             <h3 class="text-4xl mb-6">Pilares da nossa empresa</h3>
         </div>
         <div class="flex flex-row justify-evenly">
-            <div class="border w-3/12 h-96 px-12 py-12 rounded-xl hover:shadow-2xl hover:scale-110 hover:-translate-y-1 duration-300">
+            <div class="border w-3/12 h-96 px-12 py-12 rounded-xl hover:shadow-2xl hover:scale-110 hover:-translate-y-1 duration-500">
                 <div class="flex justify-center w-full h-1/6">
                     <i class="fa-solid fa-flag text-4xl mb-4"></i>
                 </div>
@@ -27,7 +27,7 @@
                     <h4 class="text-2xl">Missão</h4>
                 </div>
                 <div class="h-5/6">
-                    <p>
+                    <p class="text-center">
                         Desenvolver projetos, equipamentos e serviços aplicando constantemente tecnologia e criatividade, buscando a satisfação permanente dos nossos clientes, valorizando nossos colaboradores e fornecedores para que aperfeiçoem constantemente suas habilidades na busca do sucesso e prosperidade da empresa, comunidade e de si mesmo.
                     </p>
                 </div>
@@ -40,7 +40,7 @@
                     <h4 class="text-2xl">Visão</h4>
                 </div>
                 <div class="h-5/6">
-                    <p class="">
+                    <p class="text-center">
                         Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras eros erat, molestie ut tincidunt et, iaculis nec libero. Maecenas id consequat eros, et semper dolor. Duis non nisi eget diam ultrices imperdiet. Cras tristique sagittis dictum.
                     </p>
                 </div>
@@ -53,7 +53,7 @@
                     <h4 class="text-2xl">Valores</h4>
                 </div>
                 <div class="h-5/6">
-                    <p>
+                    <p class="text-center">
                         Produzir equipamentos e serviços com a máxima qualidade, cumprir os prazos de entrega e custos previstos, buscar constantemente soluções com viabilidade técnica e economia, e aplicar as melhores soluções envolvendo sempre: tecnologia, criatividade e flexibilidade    
                     </p>
                 </div>
