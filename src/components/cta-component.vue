1d27cac6e71684fbcdd6e0113db54149e674d008
Pedro Oliveira <pedro120403@gmail.com>
2024-06-14 09:55:40 -0300

    Estilização de componente
    - Estilização do componente para um zoom de 100% na tela

--- src/components/cta-component.vue
+++ src/components/cta-component.vue
@@ -1,11 +1,11 @@
 <template>
-    <section class="relative my-32 mx-32 py-9 flex bg-[#F5F5F5] rounded-xl hover:bg-[#eaeaeb] hover:shadow-2xl hover:-translate-y-1 hover:scale-[1.03] duration-300">
-        <span class="text-4xl m-auto">
+    <section class="relative my-16 mx-32 py-9 flex bg-[#e8e8e8] rounded-xl hover:bg-[#e8e8e8] hover:shadow-2xl hover:-translate-y-1 hover:scale-[1.03] duration-500">
+        <span class="text-3xl m-auto">
             Gostou do que viu?<br>
             Solicite um orçamento, nossa equipe está ansiosa para trabalhar com você!
         </span>
         <!-- <router-link to="/contact"> -->
-            <button class="w-52 h-16 m-auto text-xl bg-gradient-to-br from-[#00daf3] to-[#00b7cb] rounded-lg hover:shadow-2xl duration-300">
+            <button class="w-52 h-16 m-auto text-xl bg-gradient-to-br from-[#00daf3] to-[#00b7cb] rounded-lg hover:shadow-2xl duration-500">
                 <router-link to="/contact">
                     Solicitar orçamento
                 </router-link>
