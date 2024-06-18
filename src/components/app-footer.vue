6319b7e9eea84d5d991d06e53a6009a93dbd294d
Pedro Oliveira <pedro120403@gmail.com>
2024-06-18 11:27:21 -0300

    Responsividade do footer

--- src/components/app-footer.vue
+++ src/components/app-footer.vue
@@ -1,11 +1,18 @@
 <template>
     <div class="bg-[#131313]">
-        <footer class="text-[#656567] p-24">
-            <div class="grid grid-cols-3 h-36 w-12/12 mx-auto">
-                <div class="border-r border-[#656567]">
-                    <img src="../assets/logo zetta.png" alt="" class="h-16 mt-10 mr-6">
+        <footer class="text-[#656567] p-24 sm:p-12">
+            <div class="grid grid-cols-3 h-36 w-12/12 mx-auto
+            lg:h-32 lg:text-base
+            md:h-28 md:text-sm md:grid
+            sm:h-52 sm:text-sm sm:flex sm:flex-col">
+                <div class="flex justify-center items-center px-6 
+                md:h-full md:border-r md:border-[#656567]
+                sm:w-full sm:h-2/6 sm:max-md:border-b sm:border-[#656567]">
+                    <img src="../assets/logo zetta.png" alt="" class="
+                    sm:max-md:h-full">
                 </div>
-                <div class="m-auto">
+                <div class="flex justify-center items-center m-auto md:h-full px-6
+                sm:w-full sm:h-3/6">
                     <div class="">
                         <h3>Onde estamos:</h3>
                         <a href="https://www.google.com.br/maps/place/Zettatecck+Projetos+Industriais+e+Automação/@-22.3694158,-47.4025012,17z/data=!3m1!4b1!4m6!3m5!1s0x94c877f989814a41:0x7beec8fd34f62c93!8m2!3d-22.3694208!4d-47.3999209!16s%2Fg%2F11v14mh22b?entry=ttu"
@@ -15,22 +22,29 @@
                         </a>
                     </div>
                 </div>
-                <div class="border-l border-[#656567] relative">
-                    <div class="ml-16">
-                        <span>Contato</span><br>
-                        <span><i class="fa-solid fa-envelope mr-2"></i></span>
-                        <span>teste@zettatecck.com.br</span>
+                <div class="flex flex-col justify-between md:h-full px-4 relative border-[#656567]
+                md:flex-col md:items-start md:border-l md:border-[#656567]
+                sm:w-full sm:h-3/6 sm:flex-row sm:max-md:border-t">
+                    <div class="absolute top-0 left-0
+                    md:w-full md:justify-start
+                    sm:relative sm:flex sm:items-start sm:justify-center sm:items-center sm:w-6/12">
+                        <div>
+                            <span>Contato</span><br>
+                            <span><i class="fa-solid fa-envelope mr-2"></i></span>
+                            <span>teste@zettatecck.com.br</span>
+                        </div>
                     </div>
-                    <div class="ml-16 absolute bottom-0">
-                        <span>Telefone</span><br>
-                        <span><i class="fa-solid fa-phone mr-2"></i></span>
-                        <span>(99)9999-9999</span>
+                    <div class="absolute bottom-0 right-0
+                    md:w-full md:flex md:justify-start
+                    sm:relative sm:flex sm:items-end sm:justify-center sm:items-center sm:w-6/12">
+                        <div>
+                            <span>Telefone</span><br>
+                            <span><i class="fa-solid fa-phone mr-2"></i></span>
+                            <span>(99)9999-9999</span>
+                        </div>
                     </div>
                 </div>
-                <!-- <div class="border-l border-[#656567]">
-                </div> -->
             </div>
-    
         </footer>
     </div>
 </template>
