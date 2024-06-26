a4822e98738e486027037c2831e7b73ce8d588b9
Pedro Oliveira <pedro120403@gmail.com>
2024-06-26 11:18:28 -0300

    Responsividade parte de produtos da página Home

--- src/components/home/products-sections.vue
+++ src/components/home/products-sections.vue
@@ -1,13 +1,16 @@
 <template>
-    <div class="grid gap-4 grid-cols-1 grid-rows-3 place-content-evenly mt-24">
-        <section class="grid gap-4 grid-cols-2 grid-rows-1 content-center m-auto pl-36 pr-36 pb-32 h-12/12">
-            <div v-motion-slide-left :duration="1000" class="flex flex-col w-12/12 relative">
-                <h3 class="text-6xl mb-6">
+    <div class="grid gap-4 grid-cols-1 place-content-evenly mt-24">
+        <section class="flex items-center gap-4 m-auto px-20 pb-32 h-full w-full
+        sm:max-lg:flex-col sm:max-lg:pb-24">
+            <div v-motion-slide-left :duration="1000" class="flex flex-col w-full h-full relative lg:w-6/12 sm:max-lg:h-4/6">
+                <h3 class="text-6xl mb-6 sm:max-xl:text-5xl">
                     Painéis elétricos
                 </h3>
-                <p class="text-xl pb-12">Lorem ipsum dolor sit amet consectetur adipiscing elit. Vivamus eu iaculis velit, in malesuada odio. Morbi nec ligula ut lectus convallis efficitur. Nulla hendrerit mattis arcu, ac molestie enim tempor sed. Integer dui lectus, molestie eu varius vel, dignissim at odio. Ut iaculis vestibulum nisl placerat gravida. Donec eget tempor metus, sit amet aliquet tellus.</p>
+                <p class="text-xl pb-12 sm:max-xl:text-lg">
+                    Painéis elétricos são fundamentais para eficiência e controle. Na Zettatecck, oferecemos uma gama completa de alta qualidade, projetados para aplicações industriais e comerciais. Com tecnologia avançada para máxima eficiência energética e conformidade com padrões de segurança rigorosos, garantimos confiabilidade e redução de tempo de inatividade. Descubra a solução ideal para seu negócio conosco.
+                </p>
                 <router-link to="/panels">
-                    <button class="absolute bottom-0 right-0 overflow-hidden w-32 p-2 h-12 bg-black text-white border-none rounded-md text-xl font-bold cursor-pointer z-10 group">
+                    <button class="absolute bottom-0 sm:max-lg:bottom-5 right-0 overflow-hidden w-32 p-2 h-12 bg-black text-white border-none rounded-md text-xl font-bold cursor-pointer z-10 group">
                         Saiba mais!
                         <span class="absolute w-36 h-32 -top-8 -left-2 bg-white rotate-12 transform scale-x-0 group-hover:scale-x-100 transition-transform group-hover:duration-500 duration-1000 origin-left"></span>
                         <span class="absolute w-36 h-32 -top-8 -left-2 bg-cyan-400 rotate-12 transform scale-x-0 group-hover:scale-x-100 transition-transform group-hover:duration-700 duration-700 origin-left"></span>
@@ -18,21 +21,28 @@
                     </button>
                 </router-link>
             </div>
-            <div v-motion-slide-right :duration="1000" class="h-80 border-2 border-cyan-500 rounded-r-full">
+            <div v-motion-slide-right :duration="1000" class="h-96 border-2 border-cyan-500 rounded-r-full
+            lg:w-6/12
+            sm:max-lg:h-80 sm:max-lg:rounded-2xl sm:max-lg:w-8/12 sm:max-lg:mx-auto">
                 <p>imagem</p>
             </div>
         </section>
-        <section class="grid gap-4 grid-cols-2 grid-rows-1 content-center m-auto pl-36 pr-36 pb-24">
-            <div v-motion-slide-visible-once-left :duration="1000" class="h-80 border-2 border-cyan-500 rounded-l-full">
+        <section class="flex items-center gap-4 m-auto px-20 pb-32 h-full w-full
+        sm:max-lg:flex-col-reverse sm:max-lg:mt-12 sm:max-lg:pb-24">
+            <div v-motion-slide-visible-once-left :duration="1000" class="h-96 border-2 border-cyan-500 rounded-l-full
+            lg:w-6/12
+            sm:max-lg:h-80 sm:max-lg:rounded-2xl mx-auto sm:max-lg:w-8/12 sm:max-lg:mx-auto">
                 <p>imagem</p>
             </div>
-            <div v-motion-slide-visible-once-right :duration="1000" class="w-12/12 relative ">
-                <h3 class="text-6xl mb-6">
+            <div v-motion-slide-visible-once-right :duration="1000" class="w-full h-full relative sm:max-lg:h-4/6 lg:w-6/12">
+                <h3 class="text-6xl mb-6 sm:max-xl:text-5xl">
                     Projetos elétricos
                 </h3>
-                <p class="text-xl pb-12">Lorem ipsum dolor sit amet consectetur adipiscing elit. Vivamus eu iaculis velit, in malesuada odio. Morbi nec ligula ut lectus convallis efficitur. Nulla hendrerit mattis arcu, ac molestie enim tempor sed. Integer dui lectus, molestie eu varius vel, dignissim at odio. Ut iaculis vestibulum nisl placerat gravida. Donec eget tempor metus, sit amet aliquet tellus.</p>
+                <p class="text-xl pb-12 sm:max-xl:text-lg">
+                    Nossos projetos elétricos industriais são projetados sob medida para atender às necessidades específicas de cada cliente, garantindo eficiência e segurança em ambientes industriais. Com tecnologia avançada e estrita conformidade com normas de segurança, oferecemos soluções que proporcionam alta performance e confiabilidade. Descubra como podemos otimizar seus processos industriais com nossos serviços especializados em projetos elétricos.
+                </p>
                 <router-link to="/projects">
-                    <button class="absolute bottom-0 right-0 overflow-hidden w-32 p-2 h-12 bg-black text-white border-none rounded-md text-xl font-bold cursor-pointer z-10 group">
+                    <button class="absolute bottom-0 sm:max-lg:bottom-5 right-0 overflow-hidden w-32 p-2 h-12 bg-black text-white border-none rounded-md text-xl font-bold cursor-pointer z-10 group">
                         Saiba mais!
                         <span class="absolute w-36 h-32 -top-8 -left-2 bg-white rotate-12 transform scale-x-0 group-hover:scale-x-100 transition-transform group-hover:duration-500 duration-1000 origin-left"></span>
                         <span class="absolute w-36 h-32 -top-8 -left-2 bg-cyan-400 rotate-12 transform scale-x-0 group-hover:scale-x-100 transition-transform group-hover:duration-700 duration-700 origin-left"></span>
@@ -42,21 +52,20 @@
                         </span>
                     </button>
                 </router-link>
-                <!-- <button class="absolute bottom-0 right-0 inline-flex items-center justify-center p-0.5 mb-2 me-2 overflow-hidden text-sm font-medium rounded-lg group bg-gradient-to-br from-[#00daf3] to-[#0095a5] hover:shadow-2xl hover:-translate-y-1 hover:scale-110 duration-300">
-                    <span class="relative px-5 py-2.5 transition-all ease-in duration-180 dark:bg-white rounded-md group-hover:bg-opacity-0">
-                    </span>
-                </button> -->
             </div>
             
         </section>
-        <section class="grid gap-4 grid-cols-2 grid-rows-1 content-center m-auto pl-36 pr-36 pb-24">
-            <div v-motion-slide-left :duration="1000" class="flex flex-col w-12/12 relative">
-                <h3 class="text-6xl mb-6">
+        <section class="flex items-center gap-4 m-auto px-20 pb-32 h-full w-full
+        sm:max-lg:flex-col sm:max-lg:pb-24">
+            <div v-motion-slide-left :duration="1000" class="flex flex-col w-full h-full relative sm:max-lg:h-4/6 lg:w-6/12">
+                <h3 class="text-6xl mb-6 sm:max-xl:text-5xl">
                     Robôs sinalizadores
                 </h3>
-                <p class="text-xl pb-12">Lorem ipsum dolor sit amet consectetur adipiscing elit. Vivamus eu iaculis velit, in malesuada odio. Morbi nec ligula ut lectus convallis efficitur. Nulla hendrerit mattis arcu, ac molestie enim tempor sed. Integer dui lectus, molestie eu varius vel, dignissim at odio. Ut iaculis vestibulum nisl placerat gravida. Donec eget tempor metus, sit amet aliquet tellus.</p>
+                <p class="text-xl pb-12 sm:max-xl:text-lg">
+                    Robôs sinalizadores para rodovias são essenciais para melhorar a segurança e eficiência do tráfego. Nossa linha de produtos inclui robôs avançados projetados para operar de forma autônoma, utilizando tecnologia de comunicação em tempo real e adaptação às condições do trânsito. Descubra como nossas soluções podem transformar a gestão de tráfego em suas estradas. 
+                </p>
                 <router-link to="/robots">
-                    <button class="absolute bottom-0 right-0 overflow-hidden w-32 p-2 h-12 bg-black text-white border-none rounded-md text-xl font-bold cursor-pointer z-10 group">
+                    <button class="absolute bottom-0 sm:max-lg:bottom-5 right-0 overflow-hidden w-32 p-2 h-12 bg-black text-white border-none rounded-md text-xl font-bold cursor-pointer z-10 group">
                         Saiba mais!
                         <span class="absolute w-36 h-32 -top-8 -left-2 bg-white rotate-12 transform scale-x-0 group-hover:scale-x-100 transition-transform group-hover:duration-500 duration-1000 origin-left"></span>
                         <span class="absolute w-36 h-32 -top-8 -left-2 bg-cyan-400 rotate-12 transform scale-x-0 group-hover:scale-x-100 transition-transform group-hover:duration-700 duration-700 origin-left"></span>
@@ -67,7 +76,9 @@
                     </button>
                 </router-link>
             </div>
-            <div v-motion-slide-right :duration="1000" class="h-80 border-2 border-cyan-500 rounded-r-full">
+            <div v-motion-slide-right :duration="1000" class="h-96 border-2 border-cyan-500 rounded-r-full
+            lg:w-6/12
+            sm:max-lg:h-80 sm:max-lg:rounded-2xl sm:max-lg:w-8/12 sm:max-lg:mx-auto">
                 <p>imagem</p>
             </div>
         </section>
