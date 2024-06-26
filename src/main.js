03d6f4724f070fa75b38a0a83606b82ec883a6ef
Pedro Oliveira <pedro120403@gmail.com>
2024-06-26 10:42:17 -0300

    Instalação do Tailwind
    - Instalação do tailwind via npm para substituir o uso do CDN
    - Isso foi feito para a criação de um novo breakpoint para telas mobile, que não existem na versão padrão e com usando o framework via CDN

--- src/main.js
+++ src/main.js
@@ -2,6 +2,8 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from '../src/routes'
 import { MotionPlugin } from '@vueuse/motion'
+import './assets/tailwind.css';
+
 
 const app = createApp(App)
 app.use(router)
---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    'src/components/about/about.vue',
+    'src/components/contact/contact-info.vue',
+    'src/components/contact/map-component.vue',
+    'src/components/home/partner-section.vue',
+    'src/components/home/photo-carousel.vue',
+    'src/components/home/products-sections.vue',
+    'src/components/panels/panels.vue',
+    'src/components/projects/projects.vue',
+    'src/components/robots/robots.vue',
+    'src/components/app-footer.vue',
+    'src/components/cta-component.vue',
+    'src/components/navbar.vue',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
