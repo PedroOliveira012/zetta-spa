f197d954244682752c023741d4d9c24e46fefc67
Pedro Oliveira <pedro120403@gmail.com>
2024-06-28 14:54:46 -0300

    Melhorias no footer
    - Adaptação da responsividade do footer para o breakpoint xs

--- src/components/app-footer.vue
+++ src/components/app-footer.vue
@@ -1,18 +1,18 @@
 <template>
     <div class="bg-[#131313]">
-        <footer class="text-[#656567] p-24 sm:p-12">
+        <footer class="text-[#656567] p-24 xs:p-4">
             <div class="grid grid-cols-3 h-36 w-12/12 mx-auto
             lg:h-32 lg:text-base
             md:h-28 md:text-sm md:grid
-            sm:h-52 sm:text-sm sm:flex sm:flex-col">
+            xs:h-52 xs:text-sm xs:flex xs:flex-col">
                 <div class="flex justify-center items-center px-6 
                 md:h-full md:border-r md:border-[#656567]
-                sm:w-full sm:h-2/6 sm:max-md:border-b sm:border-[#656567]">
+                xs:w-full xs:h-2/6 xs:max-md:border-b xs:border-[#656567]">
                     <img src="../assets/logo zetta.png" alt="" class="
-                    sm:max-md:h-full">
+                    xs:max-md:h-full xs:max-lg:pb-3">
                 </div>
                 <div class="flex justify-center items-center m-auto md:h-full px-6
-                sm:w-full sm:h-3/6">
+                xs:w-full xs:h-3/6">
                     <div class="">
                         <h3>Onde estamos:</h3>
                         <a href="https://www.google.com.br/maps/place/Zettatecck+Projetos+Industriais+e+Automação/@-22.3694158,-47.4025012,17z/data=!3m1!4b1!4m6!3m5!1s0x94c877f989814a41:0x7beec8fd34f62c93!8m2!3d-22.3694208!4d-47.3999209!16s%2Fg%2F11v14mh22b?entry=ttu"
@@ -24,12 +24,12 @@
                 </div>
                 <div class="flex px-4 relative border-[#656567]
                 md:max-2xl:flex-col md:justify-center md:items-center md:border-l md:border-[#656567] md:h-full
-                sm:w-full sm:h-3/6 sm:max-md:border-t">
+                xs:w-full xs:h-3/6 xs:max-md:border-t">
                     <div class="xl:w-8/12
                     lg:w-11/12
                     md:max-lg:w-full md:justify-start
-                    sm:relative sm:flex sm:items-start sm:justify-center sm:w-6/12">
-                        <div>
+                    xs:relative xs:flex xs:items-center xs:justify-center xs:w-6/12">
+                        <div class="xs:max-sm:text-sm">
                             <span>Contato</span><br>
                             <span><i class="fa-solid fa-envelope mr-2"></i></span>
                             <span>teste@zettatecck.com.br</span>
@@ -38,7 +38,7 @@
                     <div class="xl:w-8/12
                     lg:w-11/12
                     md:max-lg:w-full md:flex md:justify-start
-                    sm:relative sm:flex sm:items-end sm:justify-center sm:w-6/12">
+                    xs:relative xs:flex xs:items-center xs:justify-center xs:w-6/12">
                         <div>
                             <span>Telefone</span><br>
                             <span><i class="fa-solid fa-phone mr-2"></i></span>
