6ddf4705f45a2cad977e977eaf61b3720abad460
Pedro Oliveira <pedro120403@gmail.com>
2024-07-01 13:25:30 -0300

    Melhoria na página Contato
    - Substituição do pattern em .jpg para um em .svg
    - Adaptação da responsividade da página Contato para o breakpoint xs
    - Alteração no zoom do mapa e largura

--- src/components/contact/contact-info.vue
+++ src/components/contact/contact-info.vue
@@ -1,23 +1,25 @@
 <template>
     <div class="h-80 flex flex-col justify-center items-center
         md:max-lg:h-60
-        sm:max-md:h-52">
-        <img src="@/assets/pattern.png" alt="" class="opacity-10 -z-10">
-        <div class="absolute flex flex-col justify-center items-center w-6/12 h-96 mx-auto">
+        xs:max-md:h-52
+        ">
+        <img src="@/assets/topography.svg" alt="" class="opacity-15 w-full -z-10">
+         
+        <div class="absolute flex flex-col justify-center items-center w-6/12 h-96 mx-auto xs:max-sm:w-full">
             <h3 class="text-6xl m-8
             md:max-lg:text-5xl
-            sm:max-md:text-4xl">Fale conosco</h3>
+            xs:max-md:text-4xl xs:max-sm-w-full">Fale conosco</h3>
             <h4 class="text-xl
             md:max-lg:text-2xl
-            sm:max-md:text-xl">Entre em contato com nossa equipe</h4>
+            xs:max-md:text-xl">Entre em contato com nossa equipe</h4>
         </div>
     </div>
     <section class="flex flex-row justify-evenly mx-auto mt-10 py-4 bg-gradient-to-b from-[#00aac0] to-[#005c66] z-20
     md:max-lg:mt-5
-    sm:max-md:mt-0">
+    xs:max-md:mt-0">
         <div class="flex flex-col justify-center space-y-12 text-xl p-16 bg-white rounded-xl 
         md:max-lg:text-base md:max-lg:space-y-6 md:max-lg:p-8
-        sm:max-md:text-sm sm:max-md:space-y-6 sm:max-md:p-6">
+        xs:max-md:text-sm xs:max-md:space-y-6 xs:max-md:p-6 xs:max-sm:w-11/12">
             <div>
                 <i class="fa-solid fa-location-dot mr-2"></i>
                 <span>R. João Sierra, 245 - Distrito Industrial II, Araras-SP</span>
