f786293c687428b1bf69f077e6d89ac90d1753a9
Pedro Oliveira <pedro120403@gmail.com>
2024-08-14 14:28:43 -0300

    Atualizações no footer

--- src/components/app-footer.vue
+++ src/components/app-footer.vue
@@ -1,9 +1,9 @@
 <template>
     <div class="bg-[#131313]">
-        <footer class="text-[#656567] p-24 xs:p-4 md:p-8">
+        <footer class="text-[#656567] p-24 xs:p-4 md:max-lg:py-8 md:max-lg:px-2">
             <div class="grid grid-cols-3 h-36 w-12/12 mx-auto
             lg:h-32 lg:text-base
-            md:h-28 md:text-sm md:grid 
+            md:h-fit md:text-sm md:grid 
             xs:h-52 xs:text-sm xs:flex xs:flex-col">
                 <div class="flex justify-center items-center px-6 w-full
                 md:h-full md:border-r md:border-[#656567]
@@ -22,42 +22,47 @@
                         </a>
                     </div>
                 </div>
-                <div class="flex sm:px-4 relative border-[#656567]
-                md:max-2xl:flex-col md:justify-evenly md:items-center md:border-l md:border-[#656567] md:h-full
-                sm:justify-evenly
-                xs:w-full xs:h-3/6 xs:max-md:border-t xs:px-1 xs:max-sm:justify-between">
-                    <div class="xl:w-10/12
-                    lg:w-11/12
-                    md:max-lg:w-full md:justify-start
-                    xs:relative xs:flex xs:items-center xs:w-fit xs:justify-start xs:text-sm">
-                        <div class="xs:max-sm:text-sm">
-                            <span>Contato</span><br>
-                            <span><i class="fa-solid fa-envelope mr-2"></i></span>
-                            <a href="mailto:comercial@zettatecck.com.br">comercial@zettatecck.com.br</a><br>
-                            <span class="hover:underline decoration-solid">
-                                <router-link to="/contact">
-                                    Mais opções de contato <i class="fa-solid fa-angle-right"></i>
-                                </router-link>
-                            </span>
+                <div class="flex flex-col justify-center relative border-[#656567]
+                md:items-center md:border-l md:border-[#656567] md:h-full
+                sm:max-lg:justify-evenly sm:px-4
+                xs:w-full xs:h-3/6 xs:max-md:border-t xs:px-1 xs:max-sm:justify-evenly">
+                    <div class="flex items-start
+                    2xl:w-10/12 2xl:mx-auto
+                    md:ml-0
+                    sm:w-7/12 sm:my-2 sm:mx-auto">
+                        <span>Contato</span>    
+                    </div>
+                    <div class="flex w-full justify-between mx-auto
+                    2xl:w-10/12
+                    md:max-xl:flex-col md:w-full
+                    sm:w-7/12">
+                        <div class="xl:w-full
+                        lg:w-11/12
+                        md:max-lg:w-full md:justify-start
+                        xs:relative xs:flex xs:items-center xs:w-fit xs:justify-start xs:max-lg:text-sm">
+                            <div class="xs:max-sm:text-sm xs:max-sm:w-12/12">
+                                <span><i class="fa-solid fa-envelope mr-2 x:max-sm:hidden"></i></span>
+                                <a href="mailto:comercial@zettatecck.com.br">comercial@zettatecck.com.br</a>
+                            </div>
+                        </div>
+                        <div class="xl:w-8/12
+                        lg:w-11/12
+                        md:max-lg:w-full md:flex md:max-xl:justify-start
+                        xs:relative xs:flex xs:items-center xs:w-fit xs:justify-end xs:max-sm:w-fit xs:max-lg:text-sm">
+                            <div>
+                                <span><i class="fa-solid fa-phone mr-2 x:max-sm:hidden"></i></span>
+                                <a href="tel:+551933218400">(19) 3321-8400</a>
+                            </div>
                         </div>
                     </div>
-                    <div class="xl:w-8/12
-                    lg:w-11/12
-                    md:max-lg:w-full md:flex md:justify-start
-                    xs:relative xs:flex xs:items-center xs:w-fit xs:justify-end">
-                        <div>
-                            <span>Telefone</span><br>
-                            <span><i class="fa-solid fa-phone mr-2"></i></span>
-                            <a href="tel:+551933218400">(19) 3321-8400</a>
-                        </div>
-                    </div>
-                    <div class="xl:w-3/12
-                    lg:w-11/12
-                    md:max-lg:w-full md:flex md:justify-start
-                    xs:relative xs:flex xs:items-center xs:w-fit xs:justify-end">
-                        <div>
-                            <i class="fa-brands fa-linkedin text-3xl" style="color: #656567;"></i>
-                        </div>
+                    <div class=" text-center 
+                    sm:my-2
+                    xs:max-sm:w-full">
+                        <span class="hover:underline decoration-solid">
+                            <router-link to="/contact">
+                                Mais opções de contato <i class="fa-solid fa-angle-right"></i>
+                            </router-link>
+                        </span>
                     </div>
                 </div>
             </div>
