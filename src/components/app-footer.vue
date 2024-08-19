385cbce2acf5f357f9ca794ce9bdf9aa42f02310
Pedro Oliveira <pedro120403@gmail.com>
2024-08-19 13:57:33 -0300

    Revisão da responsividade do footer

--- src/components/app-footer.vue
+++ src/components/app-footer.vue
@@ -11,9 +11,12 @@
                     <img src="../assets/logos/logo zetta.png" alt="" class="
                     xs:max-md:h-full xs:max-lg:pb-3">
                 </div>
-                <div class="flex justify-center items-center m-auto md:h-full px-6
-                xs:w-fit xs:h-3/6">
-                    <div class="">
+                <div class="flex justify-center items-center m-auto md:h-full 
+                lg:px-12
+                md:border-[#656567] md:border-r md:px-6
+                sm:px-2 sm:py-6
+                xs:w-full xs:h-3/6 xs:px-4 xs:max-sm:border-b xs:max-sm: border-[#656567]">
+                    <div>
                         <h3>Onde estamos:</h3>
                         <a href="https://www.google.com.br/maps/place/Zettatecck+Projetos+Industriais+e+Automação/@-22.3694158,-47.4025012,17z/data=!3m1!4b1!4m6!3m5!1s0x94c877f989814a41:0x7beec8fd34f62c93!8m2!3d-22.3694208!4d-47.3999209!16s%2Fg%2F11v14mh22b?entry=ttu"
                         class="hover:text-[#cbcbce] duration-300">
@@ -22,10 +25,10 @@
                         </a>
                     </div>
                 </div>
-                <div class="flex flex-col justify-center relative border-[#656567]
-                md:items-center md:border-l md:border-[#656567] md:h-full
+                <div class="flex flex-col justify-center relative
+                md:items-center md:h-full
                 sm:max-lg:justify-evenly sm:px-4
-                xs:w-full xs:h-3/6 xs:max-md:border-t xs:px-1 xs:max-sm:justify-evenly">
+                xs:w-full xs:max-sm:mx-auto xs:h-3/6 xs:px-4 xs:max-sm:justify-evenly">
                     <div class="flex items-start
                     2xl:w-10/12 2xl:mx-auto
                     md:ml-0
@@ -37,11 +40,11 @@
                     md:max-xl:flex-col md:w-full
                     sm:w-7/12">
                         <div class="xl:w-full
-                        lg:w-11/12
+                        lg:w-12/12
                         md:max-lg:w-full md:justify-start
                         xs:relative xs:flex xs:items-center xs:w-fit xs:justify-start xs:max-lg:text-sm">
                             <div class="xs:max-sm:text-sm xs:max-sm:w-12/12">
-                                <span><i class="fa-solid fa-envelope mr-2 x:max-sm:hidden"></i></span>
+                                <span><i class="fa-solid fa-envelope mr-2 xs:max-sm:hidden"></i></span>
                                 <a href="mailto:comercial@zettatecck.com.br">comercial@zettatecck.com.br</a>
                             </div>
                         </div>
@@ -50,7 +53,7 @@
                         md:max-lg:w-full md:flex md:max-xl:justify-start
                         xs:relative xs:flex xs:items-center xs:w-fit xs:justify-end xs:max-sm:w-fit xs:max-lg:text-sm">
                             <div>
-                                <span><i class="fa-solid fa-phone mr-2 x:max-sm:hidden"></i></span>
+                                <span><i class="fa-solid fa-phone mr-2 xs:max-sm:hidden"></i></span>
                                 <a href="tel:+551933218400">(19) 3321-8400</a>
                             </div>
                         </div>
